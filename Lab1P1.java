34e1c6192781218f0ae7fb9f19e7ea73b3605b6d
Bindumainam <bindumainam19@gmail.com>
2025-09-03 11:31:00 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,23 @@
+class Lab1P1{
+	public static void main(String...args){
+		short s = 12;
+		Short ss = s; //(Autoboxing)
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.BYTES);
+		System.out.println("----Integer----");
+		
+		int i = 12;
+		Integer ii = i; //(Autoboxing)
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+		System.out.println(ii.BYTES);
+		System.out.println("-----Boolean----");	
+		
+	
+	}
+}
